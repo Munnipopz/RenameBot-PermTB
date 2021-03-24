6a747e1410b20759b0975ab0cf09e2dd441bebbb
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-03-24 22:16:55 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -77,7 +77,7 @@ async def rename_cb(bot, update):
         text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below 😇".format(filename),
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="📝 RENAME 📝", callback_data="rename_button")],
                                                 [InlineKeyboardButton(text="✖️ CANCEL ✖️", callback_data="cancel_e")],
-                                                [InlineKeyboardButton(text="😈JOIN😈", url="https://t.me/mpazaan")]]),
+                                                [InlineKeyboardButton(text="😈JOIN😈", url="https://t.me/mpazaanbot")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True   
